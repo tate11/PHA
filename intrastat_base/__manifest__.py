f6ff80581e076f3839d522155fbab93bd4d91fa6
IMANE RAHIL <imanerahil2017@gmail.com>
2018-01-22 10:43:59 +0000

    [ADD]intrastat_base: modifier version 11 manifest

--- intrastat_base/__manifest__.py
+++ intrastat_base/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Intrastat Reporting Base',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Intrastat',
     'license': 'AGPL-3',
     'summary': 'Base module for Intrastat reporting',
