e4e3db97582768f87d485777da06e73661798dc5
imanrahil <irahil@agilorg.com>
2018-02-28 09:32:19 +0000

    [ADD] Ajout Module product_modification_alert

--- l10n_fr_siret/__manifest__.py
+++ l10n_fr_siret/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French company identity numbers SIRET/SIREN/NIC',
+    'name': 'French company identity numbers SIRET/SIREN/NIC agilorg',
     'version': '10.0.1.0.0',
     "category": 'French Localization',
     'author': u'Numérigraphe,Akretion,Odoo Community Association (OCA)  ',
---
+++ product_modification_alert/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Product Modification Alert',
+    'version': '11.0.1.0.0',
+    'category': 'Product Management',
+
+    'summary': 'Product Modification Alert',
+    'author': 'AgilOrg',
+    'website': 'http://www.agilorg.com',
+    'depends': ['product'],
+    'data': [
+        #
+        # 'views/product_views.xml',
+    ],
+
+    'installable': True,
+}
