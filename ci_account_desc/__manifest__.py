5a044853a26911f5b8f6f4680a83376f5a4cf106
Tarik ARAB <tarikarab>
2018-03-14 16:41:44 +0000

    [ADD] Ajout module Formule politesse ou Titre pour les Lignes devis

---
+++ ci_account_desc/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Description',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'description': """
+        Ce module permet d'améliorer le module de vente en rajoutant des descriptions pour formule politesse ou Titre dans les Ligne devis ou facture 
+    """,
+    'sequence': 1,
+    'author': "Cadrinsitu",
+    'website': "http://www.cadrinsitu.com",
+    'category': 'Website',
+    'version': '0.1',
+    'depends': ['sale', 'product', 'website_quote'],
+    'data': [
+        'views/product_views.xml',
+        'report/sale_report_templates.xml',
+        'report/website_quote_templates.xml',
+    ],
+    'installable': True,
+}
